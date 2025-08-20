17f358467f89ad2d1ee53cafb4bf93db50c50dd4
mauricio_i_carvalho <mauricio.isaac13@gmail.com>
2025-08-19 21:14:34 -0300

    Exe

--- produtosenai-main/src/main/java/com/api/produto/controllers/LojaController.java
+++ produtosenai-main/src/main/java/com/api/produto/controllers/LojaController.java
@@ -1,2 +1,46 @@
 package com.api.produto.services;
 
+
+import com.api.produto.dtos.LojaDto;
+import com.api.produto.models.LojaModel;
+import com.api.produto.repository.LojaRepository;
+import com.api.produto.services.LojaService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/loja")
+public class LojaController{
+    private final LojaRepository lojaRepository;
+    private final LojaService lojaService;
+
+    public LojaController(LojaService lojaService, LojaRepository lojaRepository){
+
+        this.lojaService = lojaService;
+        this.lojaRepository = lojaRepository;
+    }
+
+    @PostMapping("/salvar")
+    public ResponseEntity<?> salvar(
+            @RequestBody @Valid LojaDto lojaDto(){
+                LojaModel lojaModel = produtoService.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(LojaSalvar);
+
+    }
+    )
+
+
+
+
+
+
+}
+
